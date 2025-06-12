1916d995e70a85b8d35b9458a01f4b909b22138d
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2025-06-13 01:45:32 +0530

    Initial setup

---
+++ src/main/java/com/odo/b2b/backend/ODO_B2B/OdoB2BApplication.java
@@ -0,0 +1,13 @@
+package com.odo.b2b.backend.ODO_B2B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdoB2BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdoB2BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odo/b2b/backend/ODO_B2B/OdoB2BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odo.b2b.backend.ODO_B2B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoB2BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
